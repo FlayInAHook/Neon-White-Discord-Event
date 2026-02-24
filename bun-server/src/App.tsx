edf9363bae8a6a9d3f271b11cf7db4ddf9af2b8e
FlayInAHook <timmae2001@gmail.com>
2026-02-24 16:14:24 +0100

    removed database

--- bun-server/src/App.tsx
+++ bun-server/src/App.tsx
@@ -1,11 +1,18 @@
 import "./index.css";
 import { Leaderboard } from "./Leaderboard";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export function App() {
   return (
-    <div className="container mx-auto p-8 relative z-10">
-      <Leaderboard />
-    </div>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <div className="container mx-auto p-8 relative z-10">
+        <div className="absolute top-4 right-4">
+          <ModeToggle />
+        </div>
+        <Leaderboard />
+      </div>
+    </ThemeProvider>
   );
 }
 
